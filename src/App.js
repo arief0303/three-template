33f48a2b59be09d435b59a8a215e0405ee508bc9
Arief Raihan <arief0303@gmail.com>
2023-10-16 08:50:14 +0700

    init scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera, CameraControls, GizmoHelper, GizmoViewport } from '@react-three/drei'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container" className='h-screen w-screen'>
+      <Canvas>
+        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+        <directionalLight castShadow position={[2.5, 5, 5]} intensity={1.5} shadow-mapSize={[1024, 1024]} />
+        <CameraControls makeDefault />
+        <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
+          <GizmoViewport labelColor="white" axisHeadScale={1} />
+        </GizmoHelper>
+        <ambientLight intensity={0.5} />
+        <directionalLight color="white" position={[0, 5, 0]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
-  );
+  )
 }
 
 export default App;
+
